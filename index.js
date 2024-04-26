fd91b4aa245526a4a85ff3994a4195c5c5b54423
virang-git <ovu1303@gmail.com>
2024-04-27 00:12:58 +0530

    Add the Files

---
+++ index.js
@@ -0,0 +1,50 @@
+let userinput = document.getElementById("date");
+
+function calculateage() {
+  if (userinput.value === "") {
+    alert("Please Select A Date..!");
+    return;
+  }
+  let birthdate = new Date(userinput.value);
+
+  let d1 = birthdate.getDate();
+  let m1 = birthdate.getMonth() + 1;
+  let y1 = birthdate.getFullYear();
+
+  let today = new Date();
+
+  let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
+
+  let d3, m3, y3;
+
+  y3 = y2 - y1;
+
+  if (m2 >= m1) {
+    m3 = m2 - m1;
+  } else {
+    y3--;
+    m3 = 12 + m2 - m1;
+  }
+
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = daysinmonth(y1, m1) + d2 - d1;
+  }
+
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
+
+  document.getElementById("year").innerHTML = y3;
+  document.getElementById("month").innerHTML = m3;
+  document.getElementById("day").innerHTML = d3;
+}
+
+function daysinmonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
